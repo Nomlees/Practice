5e61878512b923adc90d020e66793e85c41845e1
Nomlees <nomlees@bk.ru>
2018-06-07 12:13:26 +0300

    Модель студента. Один ко многим. Группа/Студенты

---
+++ models/Group.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "group".
+ *
+ * @property int $ID
+ * @property string $Title
+ * @property string $Curator
+ * @property int $Change
+ *
+ * @property Student[] $students
+ */
+class Group extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'group';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['Title', 'Change'], 'required'],
+            [['Change'], 'integer'],
+            [['Title'], 'string', 'max' => 30],
+            [['Curator'], 'string', 'max' => 20],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'ID' => 'ID',
+            'Title' => 'Title',
+            'Curator' => 'Curator',
+            'Change' => 'Change',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getStudents()
+    {
+        return $this->hasMany(Student::className(), ['group_id' => 'ID']);
+    }
+}
---
+++ models/Student.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "Student".
+ *
+ * @property int $ID
+ * @property string $Name
+ * @property string $Email
+ * @property string $Surname
+ * @property int $Code
+ * @property int $group_id
+ *
+ * @property Group $group
+ */
+class Student extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'Student';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['Name', 'group_id'], 'required'],
+            [['Code', 'group_id'], 'integer'],
+            [['Name', 'Email', 'Surname'], 'string', 'max' => 65],
+            [['group_id'], 'exist', 'skipOnError' => true, 'targetClass' => Group::className(), 'targetAttribute' => ['group_id' => 'ID']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'ID' => 'ID',
+            'Name' => 'Name',
+            'Email' => 'Email',
+            'Surname' => 'Surname',
+            'Code' => 'Code',
+            'group_id' => 'Group ID',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getGroup()
+    {
+        return $this->hasOne(Group::className(), ['ID' => 'group_id']);
+    }
+}
